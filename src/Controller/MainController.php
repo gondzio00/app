31c06a64d13c7131ce13ea1fb924b490c2f0e484
tgondek <tgondek@chemenergia.com>
2020-03-12 21:02:33 +0100

    app

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,23 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
     public function index()
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-        ]);
-    }
-
-    public function form()
-    {
-        $person = new Persons();
-
-        $form = $this->createForm(PersonType::class, $person);
-
-        return $this->render('persons/new.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 }
